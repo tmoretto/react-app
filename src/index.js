019c6be3ff2fbf3857219d76c8d974676874d713
Tiago Moretto <moretto.tiago@gmail.com>
2018-12-29 21:27:50 -0200

    Html component ...

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = function() {
-    return <div>Hi there!</div>;
+const App = () => {
+    const buttonText = {text: 'Click Me'};
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter Name:</label>
+            <input id="name" type="text"></input>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
